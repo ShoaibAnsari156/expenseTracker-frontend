d28c78ec15d1750915a26cc897e23ab8ae12bf6e
ShoaibAnsari156 <shoaibansari6500@gmail.com>
2026-03-24 18:09:36 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import { Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import RootLayout from './layout/RootLayout'
+function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+      </Route>
+    )
+  )
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
